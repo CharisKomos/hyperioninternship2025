c97febe1233765a6fb9b36b62677207d08fe5cb4
Charalambos Komodromos <KomodromosC@indanieli.com>
2025-07-24 14:18:08 +0300

    Improved RM assignments to _billet and reduced decimal points on calculations

--- MA_Simulator/TrackingPositions/RmTrackingPosition.cs
+++ MA_Simulator/TrackingPositions/RmTrackingPosition.cs
@@ -2,6 +2,7 @@
 using MA_Simulator.Models;
 using MA_Simulator.TrackingPositions.Base;
 using MA_Simulator.TrackingPositions.Models;
+using System.Runtime.InteropServices;
 
 namespace MA_Simulator.TrackingPositions
 {
@@ -18,29 +19,41 @@ namespace MA_Simulator.TrackingPositions
         {
             base.Accept(billet);
 
-            billet.Status = BilletStatus.EnteringRM;
+            _billet!.Status = BilletStatus.EnteringRM;
+            Console.WriteLine($"The billet {_billet.PlcSemiproductCode} has status {Enum.GetName(_billet.Status)} at {PositionName}.");
         }
 
         public override void Release()
         {
-            if (_billet is TrackingBillet rollingBillet)
+            if (_billet != null)
             {
-                rollingBillet.Status = BilletStatus.ExitedRM;
-                double originalDiameter = rollingBillet.Dimension;
-                double originalLength = rollingBillet.Length;
+                _billet.Status = BilletStatus.ExitedRM;
+                Console.WriteLine($"The billet {_billet.PlcSemiproductCode} has status {Enum.GetName(_billet.Status)} at {PositionName}.");
+
+                double originalDiameter = _billet.Dimension;
+                double originalLength = _billet.Length;
 
                 // Apply percentile reduction to diameter
-                double newDiameter = originalDiameter * (1 - _decreaseFactorDimension);
-                rollingBillet.Dimension = newDiameter;
+                _billet.Dimension = originalDiameter * (1 - _decreaseFactorDimension);
 
                 // Maintain constant volume for a cylinder: V = π * r² * h
                 // So: newLength = originalVolume / (π * (newDiameter/2)²)
-                double originalVolume = Math.PI * Math.Pow(originalDiameter / 2, 2) * originalLength;
-                double newLength = originalVolume / (Math.PI * Math.Pow(newDiameter / 2, 2));
-                rollingBillet.Length = newLength;
+                double originalVolume = CalculateVolume(originalDiameter, originalLength);
 
-                base.Release();
+                _billet.Length = CalculateLength(originalVolume, _billet.Dimension);
             }
+
+            base.Release();
+        }
+
+        private double CalculateLength(double volume, double diameter)
+        {
+            return Math.Round((volume / (Math.PI * Math.Pow(diameter / 2, 2))) + 0.005, 2);
+        }
+
+        private static double CalculateVolume(double diameter, double length)
+        {
+            return Math.Round((Math.PI * Math.Pow(diameter / 2, 2) * length) + 0.005, 2);
         }
 
         public override void ConstructMesssage()
